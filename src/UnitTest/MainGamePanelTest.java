16dc073d10926077b89bc4f42040ff117cf47609
unknown <rfp30@georgetown.edu>
2015-04-28 13:39:17 -0400

    UnitTestFolder

---
+++ src/UnitTest/MainGamePanelTest.java
@@ -0,0 +1,19 @@
+package UnitTest;
+
+
+//Junit Test imports
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+
+public class MainGamePanelTest {
+	@Test
+	public void testDistance()
+	{
+
+		assertEquals(11,11);
+	}
+
+
+}

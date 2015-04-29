7e4bc93501454fea5e407358be5e110c49865b39
unknown <rfp30@georgetown.edu>
2015-04-28 21:36:23 -0400

    Select1

--- src/unitTests/SelectPanelTest.java
+++ src/unitTests/SelectPanelTest.java
@@ -2,8 +2,46 @@ package unitTests;
 
 //Junit Test imports
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import javax.swing.JPanel;
+
+import main.Game;
+import main.Level;
+
+import org.junit.Test;
+
+import panels.MainGamePanel;
+import panels.SelectPanel;
+import panels.StratPanel;
+
 public class SelectPanelTest {
+	
+	StratPanel stratPanel; 
+	MainGamePanel mainPanel;
+	
+	@Before
+	public void setUp()
+	{
+		Game g = new Game(); 
+		stratPanel = new StratPanel(g); 
+		mainPanel = new MainGamePanel(g, false);
+	}
+	
+	@Test
+	public void testNumberOfFunctions(){
+		mainPanel.setCurrentLevelIndex(1);
+		
+		ArrayList<Level> testlevels = new ArrayList<Level>();
+		testlevels = mainPanel.getLevels();
+		int expected = testlevels.get(1).getAvailableMoves().size();
+		assertEquals(2,expected,0);
+		
+		
+	}
 
 }
